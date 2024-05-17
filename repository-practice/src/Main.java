ee526915a11faa16c7e2a11be6d575fde1ec9547
ian <somnii006@gmail.com>
2024-05-16 21:56:48 -0300

    nueva subida

--- repository-practice/src/Main.java
+++ repository-practice/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello my beautyfull world!!!!!");
+        System.out.println("Now im doing another things");
     }
 }
